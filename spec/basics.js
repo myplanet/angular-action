1be45690f894018a8b90fb026036ad259c82fb87
Nick Matantsev <nick.m@myplanetdigital.com>
2014-10-28 00:01:03 -0400

    Extended basic test

--- spec/basics.js
+++ spec/basics.js
@@ -1,7 +1,7 @@
 describe('angular-action do/parameter directive', function () {
     var dom, scope, subScope;
 
-    beforeEach(module('action'));
+    beforeEach(module('mp.action'));
 
     beforeEach(inject(function($rootScope, $compile) {
         dom = angular.element('<div do="testAction(data)"><div ng-init="captureSubscope()"></div></div>');
@@ -21,5 +21,13 @@ describe('angular-action do/parameter directive', function () {
         expect(subScope.$actionHasParameterErrors).toBe(false);
         expect(subScope.$actionInvoke).toEqual(jasmine.any(Function));
         expect(subScope.$actionReset).toEqual(jasmine.any(Function));
+    });
+
+    it('invokes the action expression when "$actionInvoke" is called', function () {
+        scope.testAction = jasmine.createSpy('testAction');
+
+        subScope.$apply(function () { subScope.$actionInvoke(); });
+
+        expect(scope.testAction).toHaveBeenCalledWith({});
     });
 });
