96fe910e1e992186b9b284f198214b9f9ab757c6
Nick Matantsev <nick.matantsev@gmail.com>
2015-07-11 14:47:28 -0400

    Added interactive mock response form to sample

--- example/example.js
+++ example/example.js
@@ -2,15 +2,30 @@ var exampleApp = angular.module('ExampleApp', [
     'mp.action'
 ]);
 
-exampleApp.controller('ExamplePage', function ($scope) {
+exampleApp.controller('ExamplePage', function ($scope, $q) {
     $scope.log = [];
 
     $scope.exampleAction = function (data) {
+        var deferred = $q.defer();
+
         $scope.log.push({
-            message: 'called action function with: ' + JSON.stringify(data)
+            message: 'called action function with: ' + JSON.stringify(data),
         });
 
-        return 'result';
+        $scope.log.push({
+            message: 'waiting for promised response (e.g. AJAX)',
+            collectInput: function (value) {
+                this.collectInput = null; // no need for more input
+
+                $scope.log.push({
+                    message: 'promised response was: ' + JSON.stringify(value)
+                });
+
+                deferred.resolve(value);
+            }
+        });
+
+        return deferred.promise;
     };
 
     $scope.examplePostAction = function (value) {
